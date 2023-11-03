5dae3a50643cb2baaef14a3e36502ae6509973bb
Thalles Campagnani <thallescampagnani@gmail.com>
2023-11-03 00:40:02 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
-#include <serialport.h> // libserialport
+#include <libserialport.h>
 
-int main() {
-    // Define the serial port name (e.g., "/dev/ttyS0" for the first serial port)
-    const char* portname = "/dev/ttyS0";
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <serial_port_name>" << std::endl;
+        return 1;
+    }
+
+    const char* portname = argv[1];
 
     // Create a serial port handle
     struct sp_port* port;
@@ -29,30 +33,31 @@ int main() {
     sp_set_stopbits(port, 1);
     sp_set_parity(port, SP_PARITY_NONE);
 
-    // Set the timeout for reading
-    sp_set_timeout(port, 100); // 100 milliseconds
-
-    // Read data from the serial port
+    // Read data from the serial port with a timeout
     char header[2];
     char data[41];
     char crc[2];
 
-    result = sp_blocking_read(port, header, sizeof(header), 0);
+    const int readTimeoutMillis = 100; // Set your desired timeout in milliseconds
+
+    // Read header
+    result = sp_blocking_read(port, header, sizeof(header), readTimeoutMillis);
     if (result != 2) {
         std::cerr << "Error: Failed to read header" << std::endl;
     } else {
-        result = sp_blocking_read(port, data, sizeof(data), 0);
+        // Read data
+        result = sp_blocking_read(port, data, sizeof(data), readTimeoutMillis);
         if (result != 41) {
             std::cerr << "Error: Failed to read data" << std::endl;
         } else {
-            result = sp_blocking_read(port, crc, sizeof(crc), 0);
+            // Read CRC
+            result = sp_blocking_read(port, crc, sizeof(crc), readTimeoutMillis);
             if (result != 2) {
                 std::cerr << "Error: Failed to read CRC" << std::endl;
             } else {
                 // Check CRC and process the received data here
                 // You'll need to implement CRC checking based on your CRC algorithm
                 // and further processing of the data as needed.
-                // You can use libraries like "libcrc" to calculate and check CRC.
 
                 std::cout << "Received header: " << header << std::endl;
                 std::cout << "Received data: " << data << std::endl;
